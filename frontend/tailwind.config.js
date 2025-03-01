149c1ee46addde5583fbe991ce95a7edc578dac4
Gourav Rajak <gouravrajak+github@gmail.com>
2025-03-02 02:17:39 +0530

    The final update is here

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,10 @@ const require = createRequire(import.meta.url);
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
